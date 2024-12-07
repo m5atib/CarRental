3f0679b22635268e0d394fc7303865e56702812f
Muhaimen Khatib <khatibmuhaimen@gmail.com>
2024-12-07 16:15:48 +0200

    Create unit tests for CarRental.BL.Car

--- CarRental.BL/Program.cs
+++ CarRental.BL/Program.cs
@@ -1,80 +1,215 @@
-﻿namespace CarRental.BL
+﻿using System;
+
+namespace CarRental.BL
 {
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+        }
+    }
     public class Car
     {
-        public int CarId { get; set; }
-        public string Make { get; set; } // e.g., Toyota, Ford
-        public string Model { get; set; } // e.g 2024, 2010
-        public int Year { get; set; }
-        public string LicensePlate { get; set; }
-        public decimal RentalPricePerDay { get; set; }
-        public bool IsAvailable { get; set; }
+        public int CarId
+        {
+            get; set;
+        }
+        public string Make
+        {
+            get; set;
+        } // e.g., Toyota, Ford
+        public string Model
+        {
+            get; set;
+        } // e.g 2024, 2010
+        public int Year
+        {
+            get; set;
+        }
+        public string LicensePlate
+        {
+            get; set;
+        }
+        public decimal RentalPricePerDay
+        {
+            get; set;
+        }
+        public bool IsAvailable
+        {
+            get; set;
+        }
     }
 
     public class Customer
     {
-        public int CustomerId { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Email { get; set; }
-        public string PhoneNumber { get; set; }
-        public string DriverLicenseNumber { get; set; }
+        public int CustomerId
+        {
+            get; set;
+        }
+        public string FirstName
+        {
+            get; set;
+        }
+        public string LastName
+        {
+            get; set;
+        }
+        public string Email
+        {
+            get; set;
+        }
+        public string PhoneNumber
+        {
+            get; set;
+        }
+        public string DriverLicenseNumber
+        {
+            get; set;
+        }
     }
 
     public class Rental
     {
-        public int RentalId { get; set; }
-        public int CarId { get; set; }
-        public int CustomerId { get; set; }
-        public DateTime RentalStartDate { get; set; }
-        public DateTime RentalEndDate { get; set; }
-        public decimal TotalCost { get; set; }
+        public int RentalId
+        {
+            get; set;
+        }
+        public int CarId
+        {
+            get; set;
+        }
+        public int CustomerId
+        {
+            get; set;
+        }
+        public DateTime RentalStartDate
+        {
+            get; set;
+        }
+        public DateTime RentalEndDate
+        {
+            get; set;
+        }
+        public decimal TotalCost
+        {
+            get; set;
+        }
 
         // Navigation Properties
-        public Car Car { get; set; }
-        public Customer Customer { get; set; }
+        public Car Car
+        {
+            get; set;
+        }
+        public Customer Customer
+        {
+            get; set;
+        }
     }
 
     public class Payment
     {
-        public int PaymentId { get; set; }
-        public int RentalId { get; set; }
-        public decimal Amount { get; set; }
-        public DateTime PaymentDate { get; set; }
-        public string PaymentMethod { get; set; } // e.g., Credit Card, Cash
+        public int PaymentId
+        {
+            get; set;
+        }
+        public int RentalId
+        {
+            get; set;
+        }
+        public decimal Amount
+        {
+            get; set;
+        }
+        public DateTime PaymentDate
+        {
+            get; set;
+        }
+        public string PaymentMethod
+        {
+            get; set;
+        } // e.g., Credit Card, Cash
 
         // Navigation Property
-        public Rental Rental { get; set; }
+        public Rental Rental
+        {
+            get; set;
+        }
     }
 
     public class Employee
     {
-        public int EmployeeId { get; set; }
-        public string Name { get; set; }
-        public string Role { get; set; } // e.g., Manager, Clerk
-        public string Email { get; set; }
-        public string PhoneNumber { get; set; }
+        public int EmployeeId
+        {
+            get; set;
+        }
+        public string Name
+        {
+            get; set;
+        }
+        public string Role
+        {
+            get; set;
+        } // e.g., Manager, Clerk
+        public string Email
+        {
+            get; set;
+        }
+        public string PhoneNumber
+        {
+            get; set;
+        }
     }
 
 
     public class Branch
     {
-        public int BranchId { get; set; }
-        public string Name { get; set; }
-        public string Address { get; set; }
-        public string PhoneNumber { get; set; }
+        public int BranchId
+        {
+            get; set;
+        }
+        public string Name
+        {
+            get; set;
+        }
+        public string Address
+        {
+            get; set;
+        }
+        public string PhoneNumber
+        {
+            get; set;
+        }
     }
 
     public class CarMaintenance
     {
-        public int MaintenanceId { get; set; }
-        public int CarId { get; set; }
-        public DateTime MaintenanceDate { get; set; }
-        public string Description { get; set; }
-        public decimal Cost { get; set; }
+        public int MaintenanceId
+        {
+            get; set;
+        }
+        public int CarId
+        {
+            get; set;
+        }
+        public DateTime MaintenanceDate
+        {
+            get; set;
+        }
+        public string Description
+        {
+            get; set;
+        }
+        public decimal Cost
+        {
+            get; set;
+        }
 
         // Navigation Property
-        public Car Car { get; set; }
+        public Car Car
+        {
+            get; set;
+        }
     }
 
 
---
+++ Tests/CarRental.BLTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CarRental.BLTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CarRental.BLTest")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f637950c-e752-40ad-beb0-6f484de0c616")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
